9606b98d9b468763d1f90905ef6c56bc7358fbfe
Alexander <aleksandr.chasovnikov@gmail.com>
2022-04-08 09:55:34 +0300

    admin panel

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -33,8 +33,15 @@ class RouteServiceProvider extends ServiceProvider
             //     ->middleware('api')
             //     ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
+            Route::middleware('web')            
+            // ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('admin')
+                ->prefix('admin')
+                ->name('admin.')
+                // ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
         });
     }
 
---
+++ src/routes/admin.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\AuthController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('guest:admin')->group(function() {
+    Route::get('login', [AuthController::class, 'index'])->name('login');
+    Route::post('login_process', [AuthController::class, 'login'])->name('login_process');
+});
+
+Route::middleware('auth:admin')->group(function() {
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::resource('posts', PostController::class);
+});
